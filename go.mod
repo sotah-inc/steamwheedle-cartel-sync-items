03596be041f6205cd962b5f102be951b2e1e8c8e
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-08 18:23:47 -0400

    Initial commit of build assets.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/sotah-inc/steamwheedle-cartel-sync-items
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.41.0
+	github.com/myesui/uuid v1.0.0 // indirect
+	github.com/nats-io/gnatsd v1.4.1 // indirect
+	github.com/nats-io/nkeys v0.0.2 // indirect
+	github.com/sotah-inc/steamwheedle-cartel v0.0.0-20190708221943-1eb0a14c6db9
+)
---
+++ main.go
@@ -0,0 +1,302 @@
+package steamwheedle_cartel_sync_items
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/storage"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/blizzard"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/bus"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/bus/codes"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/logging"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah/gameversions"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/state/subjects"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/store"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/store/regions"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/util"
+)
+
+var (
+	projectId = os.Getenv("GCP_PROJECT")
+
+	busClient               bus.Client
+	receiveSyncedItemsTopic *pubsub.Topic
+
+	primaryRegion sotah.Region
+
+	blizzardClient blizzard.Client
+
+	storeClient store.Client
+	itemsBase   store.ItemsBase
+	itemsBucket *storage.BucketHandle
+)
+
+func init() {
+	var err error
+	busClient, err = bus.NewClient(projectId, "fn-sync-all-items")
+	if err != nil {
+		log.Fatalf("Failed to create new bus client: %s", err.Error())
+
+		return
+	}
+	receiveSyncedItemsTopic, err = busClient.FirmTopic(string(subjects.ReceiveSyncedItems))
+	if err != nil {
+		log.Fatalf("Failed to get firm topic: %s", err.Error())
+
+		return
+	}
+
+	storeClient, err = store.NewClient(projectId)
+	if err != nil {
+		log.Fatalf("Failed to create new store client: %s", err.Error())
+
+		return
+	}
+
+	itemsBase = store.NewItemsBase(storeClient, regions.USCentral1, gameversions.Retail)
+	itemsBucket, err = itemsBase.GetFirmBucket()
+	if err != nil {
+		log.Fatalf("Failed to get firm bucket: %s", err.Error())
+
+		return
+	}
+
+	bootBase := store.NewBootBase(storeClient, regions.USCentral1)
+
+	var bootBucket *storage.BucketHandle
+	bootBucket, err = bootBase.GetFirmBucket()
+	if err != nil {
+		log.Fatalf("Failed to get firm bucket: %s", err.Error())
+
+		return
+	}
+
+	blizzardCredentials, err := bootBase.GetBlizzardCredentials(bootBucket)
+	if err != nil {
+		log.Fatalf("Failed to get blizzard-credentials: %s", err.Error())
+
+		return
+	}
+	blizzardClient, err = blizzard.NewClient(blizzardCredentials.ClientId, blizzardCredentials.ClientSecret)
+	if err != nil {
+		log.Fatalf("Failed to create blizzard client: %s", err.Error())
+
+		return
+	}
+
+	regions, err := bootBase.GetRegions(bootBucket)
+	if err != nil {
+		log.Fatalf("Failed to get regions: %s", err.Error())
+
+		return
+	}
+	primaryRegion, err = regions.GetPrimaryRegion()
+	if err != nil {
+		log.Fatalf("Failed to get primary region: %s", err.Error())
+
+		return
+	}
+}
+
+func SyncExistingItem(id blizzard.ItemID) (string, error) {
+	itemObj, err := itemsBase.GetFirmObject(id, itemsBucket)
+	if err != nil {
+		return "", err
+	}
+
+	item, err := itemsBase.NewItem(itemObj)
+	if err != nil {
+		return "", err
+	}
+
+	if item.NormalizedName != "" {
+		return item.NormalizedName, nil
+	}
+
+	normalizedName, err := sotah.NormalizeName(item.Name)
+	if err != nil {
+		return "", err
+	}
+	item.NormalizedName = normalizedName
+
+	if err := itemsBase.WriteItem(itemObj, item); err != nil {
+		return "", err
+	}
+
+	return normalizedName, nil
+}
+
+func SyncItem(id blizzard.ItemID) (string, error) {
+	itemObj := itemsBase.GetObject(id, itemsBucket)
+
+	exists, err := itemsBase.ObjectExists(itemObj)
+	if err != nil {
+		return "", err
+	}
+	if exists {
+		logging.WithField("id", id).Info("Item already exists, calling func for existing item")
+
+		return SyncExistingItem(id)
+	}
+
+	logging.WithField("id", id).Info("Downloading")
+	uri, err := blizzardClient.AppendAccessToken(blizzard.DefaultGetItemURL(primaryRegion.Hostname, id))
+	if err != nil {
+		return "", err
+	}
+
+	respMeta, err := blizzard.Download(uri)
+	if err != nil {
+		return "", err
+	}
+	if respMeta.Status != http.StatusOK {
+		return "", errors.New("status was not OK")
+	}
+
+	logging.WithField("id", id).Info("Parsing and encoding")
+	blizzardItem, err := blizzard.NewItem(respMeta.Body)
+	if err != nil {
+		return "", err
+	}
+	item := sotah.Item{Item: blizzardItem}
+
+	normalizedName, err := sotah.NormalizeName(item.Name)
+	if err != nil {
+		return "", err
+	}
+	item.NormalizedName = normalizedName
+
+	// writing it out to the gcloud object
+	logging.WithField("id", id).Info("Writing to items-base")
+
+	if err := itemsBase.WriteItem(itemObj, item); err != nil {
+		return "", err
+	}
+
+	return normalizedName, nil
+}
+
+type HandleIdsJob struct {
+	Err            error
+	Id             blizzard.ItemID
+	NormalizedName string
+}
+
+func HandleIds(ids blizzard.ItemIds) (sotah.ItemIdNameMap, error) {
+	// spawning workers
+	in := make(chan blizzard.ItemID)
+	out := make(chan HandleIdsJob)
+	worker := func() {
+		for id := range in {
+			normalizedName, err := SyncItem(id)
+			if err != nil {
+				out <- HandleIdsJob{
+					Err: err,
+					Id:  id,
+				}
+
+				continue
+			}
+
+			out <- HandleIdsJob{
+				Err:            nil,
+				Id:             id,
+				NormalizedName: normalizedName,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(8, worker, postWork)
+
+	// spinning it up
+	go func() {
+		for _, id := range ids {
+			in <- id
+		}
+
+		close(in)
+	}()
+
+	// waiting for the results to drain out
+	results := sotah.ItemIdNameMap{}
+	for outJob := range out {
+		if outJob.Err != nil {
+			logging.WithField("error", outJob.Err.Error()).Error("Failed to sync item")
+
+			continue
+		}
+
+		results[outJob.Id] = outJob.NormalizedName
+	}
+
+	return results, nil
+}
+
+func Handle(in bus.Message) bus.Message {
+	m := bus.NewMessage()
+
+	itemIds, err := blizzard.NewItemIds(in.Data)
+	if err != nil {
+		m.Err = err.Error()
+		m.Code = codes.GenericError
+
+		return m
+	}
+
+	results, err := HandleIds(itemIds)
+	if err != nil {
+		m.Err = err.Error()
+		m.Code = codes.GenericError
+
+		return m
+	}
+
+	data, err := results.EncodeForDelivery()
+	if err != nil {
+		m.Err = err.Error()
+		m.Code = codes.GenericError
+
+		return m
+	}
+
+	logging.WithField("results", len(results)).Info("Received synced items payload, pushing to receive-synced-items topic")
+	msg := bus.NewMessage()
+	msg.Data = data
+	if _, err := busClient.Publish(receiveSyncedItemsTopic, msg); err != nil {
+		m.Err = err.Error()
+		m.Code = codes.GenericError
+
+		return m
+	}
+
+	m.Code = codes.Ok
+
+	return m
+}
+
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+func SyncItems(_ context.Context, m PubSubMessage) error {
+	var in bus.Message
+	if err := json.Unmarshal(m.Data, &in); err != nil {
+		return err
+	}
+
+	msg := Handle(in)
+	msg.ReplyToId = in.ReplyToId
+	if _, err := busClient.ReplyTo(in, msg); err != nil {
+		return err
+	}
+
+	return nil
+}
